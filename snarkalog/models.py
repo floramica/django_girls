c9f2ec96be66d167afab85eee4624d3ceddd8117
Gabrielle <5787687+gabriellemic@users.noreply.github.com>
2019-03-16 17:09:51 +0100

    create unicorn rainbow scoring

--- snarkalog/models.py
+++ snarkalog/models.py
@@ -9,6 +9,8 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    score = models.IntegerField()
+    uniscore = models.IntegerField()
 
     def publish(self):
         self.published_date = timezone.now()
